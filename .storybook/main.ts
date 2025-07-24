a539201cdf8d1781bc2edda0502dee662efe0540
David Burles <dburles@gmail.com>
2025-07-24 10:52:50 +1000

    force crash

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,6 +2,8 @@ import type { StorybookConfig } from "@storybook/react-vite";
 
 console.log(`Node ${process.version}`);
 
+throw Error("force crash");
+
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
